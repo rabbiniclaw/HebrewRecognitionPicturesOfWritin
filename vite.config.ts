ca9debbceceb64ae3270c6930ae63cce30bfbfa9
rabbiniclaw <rabbiniclaws@gmail.com>
2026-05-29 09:45:19 +0300

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // This is crucial for GitHub Pages. It tells Vite to use relative paths.
-  base: './', 
-});
